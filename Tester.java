4c328898626d433d72c449f91414ae3b4405218b
Anindya Chatterjee <anindya.wbut@gmail.com>
2018-06-14 01:27:46 +0530

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,11 +1,27 @@
-package structural_facade;
+package structural_adapter;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		ShapeFacade shapeFacade = new ShapeFacade();
-		shapeFacade.drawCircle();
-		shapeFacade.drawRectangle();
-		shapeFacade.drawSquare();
+		XPayment xpay = new XPaymentImpl();
+		
+		xpay.setCreditCardNo("4789565874102365");
+        xpay.setCustomerName("Anindya");
+        xpay.setCardExpMonth("09");
+        xpay.setCardExpYear("25");
+        xpay.setCardCVVNo((short)235);
+        xpay.setAmount(2565.23);
+        
+        NewPayment newPay = new XPaymentToNewPaymentAdapter(xpay);
+        testNewPayObj(newPay);
+	}
+	
+	public static void testNewPayObj(NewPayment newPay){
+		System.out.println(newPay.getCardOwnerName());
+		System.out.println(newPay.getCustCardNo());
+		System.out.println(newPay.getCardExpMonthDate());
+		System.out.println(newPay.getCVVNo());
+		System.out.println(newPay.getTotalAmount());
 	}
 }
+
